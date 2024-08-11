bae23709b78348ef545cabef44a99fa96b1cd4db
STARCK-RADOUA <122606108+STARCK-RADOUA@users.noreply.github.com>
2024-08-11 03:49:24 +0100

    noti

--- backend/models/Notification.js
+++ backend/models/Notification.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const notificationSchema = new mongoose.Schema({
   User_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  title: { type: String, required: true },
   message: { type: String, required: true },
   read_at: { type: Date, default: null },
   created_at: { type: Date, default: Date.now },
