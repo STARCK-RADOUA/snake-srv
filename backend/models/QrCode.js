20aaf6b52fb734fc5fcfcb42c05d0052b40c6258
STARCK-RADOUA <122606108+STARCK-RADOUA@users.noreply.github.com>
2024-08-26 02:52:11 +0100

    ss

---
+++ backend/models/QrCode.js
@@ -0,0 +1,13 @@
+// models/QrCode.js
+const mongoose = require('mongoose');
+
+const qrCodeSchema = new mongoose.Schema({
+  clientId: { type: String, required: true },
+  deviceId: { type: String, required: true },
+  uniqueId: { type: String, required: true, unique: true },
+  timestamp: { type: Date, required: true },
+  expirationTime: { type: Date, required: true },
+  isUsed: { type: Boolean, default: false }
+});
+
+module.exports = mongoose.model('QrCode', qrCodeSchema);
